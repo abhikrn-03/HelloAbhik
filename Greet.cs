6b4e41aeca8dbcaaebe0b00312fce37645db526b
abhikrn-03 <abhik333333@gmail.com>
2022-05-24 15:49:17 +0530

    Add project files.

---
+++ Greet.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloAbhik
+{
+    public class Greet
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Abhik says hello to {0}", name);
+        }
+
+    }
+}
